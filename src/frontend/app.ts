10e5e1cd2b42a3463a4acf1b17f5d815959619f5
Dan Dan Berendsen <dandan.berendsen@hotmail.com>
2023-05-05 17:09:49 +0200

    Basic html

---
+++ src/frontend/app.ts
@@ -0,0 +1,6 @@
+function submit() {
+    let text:String;
+    text = document.getElementById("textInput").valueOf().toString();
+    console.log("text")
+    document.getElementById("result").innerText = ":)";
+}
